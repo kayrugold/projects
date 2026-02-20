5efac102ab4e23042f383d74695076b0f093ca61
Andy Davis <kayrugold@gmail.com>
2026-02-20 14:41:23 -0800

    minor style adj to optimize books

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - The Master Switch
-const FORGE_VERSION = "4.7.8"; 
+const FORGE_VERSION = "4.7.9"; 
 const CACHE_NAME = `forge-cache-v${FORGE_VERSION}`;
 
 function getVersionedAsset(path) {
