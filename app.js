974f49d70b8d8f1fc324f278b6ceb0537bed4f18
Andy Davis <kayrugold@gmail.com>
2026-02-21 16:15:50 -0800

    forgot to add the version 4.8.1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* app.js - v4.7.9 Master Studio Logic (Journal Fixes) */
+/* app.js - v4.8.1 Master Studio Logic (Journal Fixes) */
 
 // ==========================================
 // 0. GLOBAL VARIABLES
