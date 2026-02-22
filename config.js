5010fc1ff25df9c47dca632c8c56886993ada405
Andy Davis <kayrugold@gmail.com>
2026-02-22 12:59:14 -0800

    Updated Guild Page Return Policy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - The Master Switch
-const FORGE_VERSION = "4.8.5";
+const FORGE_VERSION = "4.8.6";
 const CACHE_NAME = `forge-cache-v${FORGE_VERSION}`;
 
 function getVersionedAsset(path) {
